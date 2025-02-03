6373284c957d24d106ee93bc28bf28a9a36f9910
Abiodun001-world <adekunleabiodun354@gmail.com>
2025-02-03 22:22:03 +0100

    updated

---
+++ README.md
@@ -0,0 +1,38 @@
+# AltSchool Africa Third Semester Project 
+
+## Project Title: BookStore API
+
+### Project Description
+This project is a simple API that allows users to perform CRUD operations on a bookstore with SSO(Auth0 authentication - Log in, Log out). The API allows users to create, read, update and delete books/authors from the bookstore. All the code is written by our instructor [Rising Odegua](https://www.linkedin.com/in/risingdeveloper/) and this repo is for practice, PRACTICE! PRACTICE!! PRACTICE!!!
+
+#### Installation and Setup
+Clone the repository:
+git clone https://github.com/Abiodun001-world/bookstore-final-project.git
+
+##### Navigate to the project directory:
+cd bookstore-final-project
+
+## Install dependencies:
+npm install
+
+###### Create a .env file in the root directory:
+PORT=
+MONGO_URI=
+AUTH0_SECRET=
+AUTH0_BASE_URL=
+AUTH0_CLIENT_ID=
+AUTH0_ISSUER_BASE_URL=
+
+# Start the application:
+`npm start`
+
+Access the application via http://localhost:4000
+
+API Structure:
+[BookStore API Structure](https://hackmd.io/@aiodun001-world/Bk_XlYUdJg)
+
+API Endpoints:
+[BookStore API Documentation](https://documenter.getpostman.com/view/28730642/2sAYX5KNKR)
+
+
+
--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet")
 const CONFIG = require("./config/config");
 const connectToDb = require("./db/mongodb");
+const { requiresAuth } = require('express-openid-connect');
 const logger = require("./logging/logger");
+
+const auth0Middleware = require("./auth/auth0");
 
 // Books Route 
 const bookRouter = require("./routes/books.routes");
@@ -19,23 +22,23 @@ connectToDb();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
-
-// Apply the rate limiting middleware to all requests.
+// Apply the rate limiting middleware to all requests
 app.use(limiter)
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth0Middleware);
 
 // Security Middleware
 app.use(helmet());
 
-app.use("/api/v1/books", bookRouter);
-app.use("/api/v1/authors", authorRouter);
+app.use("/api/v1/books", requiresAuth(), bookRouter);
+app.use("/api/v1/authors", requiresAuth(), authorRouter);
 
 
 app.get("/", (req, res) => {
